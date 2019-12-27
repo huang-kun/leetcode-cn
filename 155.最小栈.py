30fb326a0bd971c4d1009da291a4cd806dd4dc58
huang-kun <jack-huang-developer@foxmail.com>
2019-12-27 17:55:37 +0800

    155.最小栈(v2)

--- 155.最小栈.py
+++ 155.最小栈.py
@@ -5,12 +5,46 @@
 #
 
 # @lc code=start
+
+# 参考了用户@Krahets的题解，恍然大悟，怎么自己就没想到呢。。
+# https://leetcode-cn.com/problems/min-stack/solution/min-stack-fu-zhu-stackfa-by-jin407891080/
 class MinStack:
 
-    # 单纯从算法的角度考虑的话，可以在每次入栈时比较和保留最小值，
-    # 而在出栈后，只有在删除了最小值的情况下，才需要重新计算剩余元素的最小值。
-    # 最坏的情况下，每次入栈的数字都是越来越小，那么每次出栈的时间复杂度为O(n)
+    def __init__(self):
+        self.list = []
+        self.mlist = []
 
+    def push(self, x: int) -> None:
+        self.list.append(x)
+        if len(self.mlist) == 0:
+            self.mlist.append(x)
+        elif self.mlist[-1] >= x:
+            self.mlist.append(x)
+
+    def pop(self) -> None:
+        x = self.list.pop()
+        if self.mlist[-1] == x:
+            self.mlist.pop()
+
+    def top(self) -> int:
+        return self.list[-1]
+
+    def getMin(self) -> int:
+        return self.mlist[-1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+# @lc code=end
+
+
+# 以下是第一个版本的代码，在每次入栈时比较和保留最小值，
+# 而在出栈后，只有在删除了最小值的情况下，才需要重新计算剩余元素的最小值。
+# 最坏的情况下，每次入栈的数字都是越来越小，那么每次出栈的时间复杂度为O(n)
+class MinStack1:
     def __init__(self):
         self.list = []
         self.min = None
@@ -31,14 +65,3 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.min
-        
-
-
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
-# @lc code=end
-
