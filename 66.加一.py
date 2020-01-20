2766a3034f4417eb802b7b6baa607137e25e759f
huang-kun <jack-huang-developer@foxmail.com>
2020-01-20 15:05:27 +0800

    66.加一(v2)

--- 66.加一.py
+++ 66.加一.py
@@ -8,8 +8,44 @@ from typing import List
 
 # @lc code=start
 class Solution:
-    
+
+    # 第二次刷题，思路依旧是整数与数组转换
     def plusOne(self, digits: List[int]) -> List[int]:
+        l = len(digits)
+        if l == 0:
+            return []
+        # 如果个位数小于9，加一后可以快速返回
+        last_digit = digits[-1]
+        if last_digit < 9:
+            last_digit += 1
+            digits[-1] = last_digit
+            return digits
+        # 将数组转换成整数
+        num = 0
+        allNines = True
+        for i in range(l):
+            digit = digits[i]
+            num += digit * (10 ** (l - i - 1))
+            if digit != 9:
+                allNines = False
+        # 整数加一
+        num += 1
+        # 如果每个数位都是9，就增加数位长度
+        if allNines:
+            l += 1
+        # 再将整数转换为数组
+        digitsPlus = []
+        for i in range(l):
+            digit = num // (10 ** (l - i - 1))
+            digitsPlus.append(digit)
+            num %= (10 ** (l - i - 1))
+        
+        return digitsPlus
+
+    
+# @lc code=end
+
+    def plusOne1(self, digits: List[int]) -> List[int]:
         """该题目可以通过将整数到位数的分解与合成，来实现整数与列表之间的转换"""
 
         # 首先快速返回一批可以直接在个位数加一的列表
