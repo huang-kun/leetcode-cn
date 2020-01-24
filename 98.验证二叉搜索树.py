324073e96f5742f1bf9dc12c1181b39f666815a2
huangkun <jack-huang-developer@foxmail.com>
2020-01-24 11:25:22 +0800

    98.验证二叉搜索树(v3)

--- 98.验证二叉搜索树.py
+++ 98.验证二叉搜索树.py
@@ -62,31 +62,59 @@ class TreeNode:
 #         self.right = None
 
 class Solution:
+    # 广度优先遍历法：
+    def isValidBST(self, root: TreeNode) -> bool:
+        """满足二叉搜索树的条件：minVal < node.val < maxVal"""
+        if root is None:
+            return True
+        
+        # 用（伪）队列来记录每个节点以及节点值的有效范围
+        first = 0
+        queue = [(root, None, None)]
+        while first < len(queue):
+            # 出队
+            node, minVal, maxVal = queue[first]
+            first += 1
+            
+            if node is None:
+                continue
+            if minVal is not None and node.val <= minVal:
+                return False
+            if maxVal is not None and node.val >= maxVal:
+                return False
+            
+            # 入队
+            queue.append((node.left, minVal, node.val))
+            queue.append((node.right, node.val, maxVal))
+        
+        return True
+
+# @lc code=end
 
     # 深度优先遍历法：
-    def isValidBST(self, root: TreeNode) -> bool:
+    def isValidBST2(self, root: TreeNode) -> bool:
         """满足二叉搜索树的条件：minVal < node.val < maxVal"""
         if root is None:
             return True
         
         # 用栈来记录每个节点以及节点值的有效范围
         stack = [(root, None, None)]
-        while len(stack) > 0:
+        while len(stack):
+            # 出栈
             node, minVal, maxVal = stack.pop()
             
+            if node is None:
+                continue
             if minVal is not None and node.val <= minVal:
                 return False
             if maxVal is not None and node.val >= maxVal:
                 return False
             
-            if node.left:
-                stack.append((node.left, minVal, node.val))
-            if node.right:
-                stack.append((node.right, node.val, maxVal))
+            # 入栈
+            stack.append((node.left, minVal, node.val))                
+            stack.append((node.right, node.val, maxVal))
         
         return True
-
-# @lc code=end
 
     # 递归法：
     def isValidBST1(self, root: TreeNode) -> bool:
@@ -96,16 +124,14 @@ class Solution:
         """满足二叉搜索树的条件：minVal < root.val < maxVal"""
         if root is None:
             return True
-        
         if minVal is not None and root.val <= minVal:
             return False
-        
         if maxVal is not None and root.val >= maxVal:
             return False
-        
+
         return self._isValidBST(root.left, minVal, root.val) and \
-                self._isValidBST(root.right, root.val, maxVal)
-        
+               self._isValidBST(root.right, root.val, maxVal)
+
 # 比如二叉搜索树：
 #  10
 #  / \
@@ -129,3 +155,77 @@ class Solution:
 #                    /       \
 #                  /           \
 #             10<[12]<15   15<[20]<null
+
+
+# -------
+# 测试用例
+# -------
+
+def indexOfLastNode(nodeList):
+    """返回最后一个不是None的节点的index"""
+    if nodeList == []:
+        return -1
+    for i in range(len(nodeList)-1, -1, -1):
+        if nodeList[i] is not None:
+            return i
+    return -1
+
+def makeNode(val):
+    """生成树节点"""
+    if val is None:
+        return None
+    return TreeNode(val)
+
+def convertTree(tree):
+    """树 -> 数组"""
+    if tree is None:
+        return []
+    
+    first = 0
+    queue = [tree]
+    while first < len(queue):
+        # dequeue
+        node = queue[first]
+        first += 1
+
+        if node is None:
+            continue
+
+        # inqueue
+        queue.append(node.left)
+        queue.append(node.right)
+    
+    # 除去尾部多余的null
+    last = indexOfLastNode(queue)
+    queue = queue[:last+1]
+    # 取出val
+    return list(map(lambda x: x if x is None else x.val, queue))
+
+def convertArray(array):
+    """数组 -> 树"""
+    if array is None or array == []:
+        return None
+
+    count = len(array)
+    nodes = list(map(lambda x: makeNode(x), array))
+    for parentIdx, parent in enumerate(nodes):
+        leftChildIdx = parentIdx * 2 + 1
+        if leftChildIdx < count:
+            leftChild = nodes[leftChildIdx]
+            parent.left = leftChild
+        rightChildIdx = parentIdx * 2 + 2
+        if rightChildIdx < count:
+            rightChild = nodes[rightChildIdx]
+            parent.right = rightChild
+
+    return nodes[0]
+
+def testCase(s, array):
+    tree = convertArray(array)
+    return s.isValidBST(tree)
+
+if __name__ == '__main__':
+    pass
+    s = Solution()
+    assert testCase(s, [2,1,3]) == True
+    assert testCase(s, [5,1,4,None,None,3,6]) == False
