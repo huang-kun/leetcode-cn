7355f678d0e5d60707cf6dbd467e3401308771a3
huang-kun <jack-huang-developer@foxmail.com>
2020-02-10 11:03:26 +0800

    236.二叉树的最近公共祖先(v4)

--- 236.二叉树的最近公共祖先.py
+++ 236.二叉树的最近公共祖先.py
@@ -20,9 +20,45 @@ class Solution:
         self.pos_q = 0
         self.ans = None
 
-    # 官方题解：递归（80ms）
-    # https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/er-cha-shu-de-zui-jin-gong-gong-zu-xian-by-leetcod/
+    # 官方题解思路：无父指针的迭代，使用栈（80 ~ 152ms）
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        # 记录节点和状态：0未遍历子节点，1已遍历左节点，2已遍历左右子节点
+        stack = [[root, 0]]
+        find_set = set()
+        lca = root
+
+        while len(stack):
+            subarr = stack[-1]
+            node = subarr[0]
+            state = subarr[1]
+
+            if node.val == p.val or node.val == q.val:
+                if node.val not in find_set:
+                    find_set.add(node.val)
+                    if len(find_set) == 2:
+                        return lca
+                    else:
+                        lca = node
+
+            if state == 2:
+                stack.pop()
+                if lca.val == node.val:
+                    lca = stack[-1][0] if len(stack) else None
+            elif state == 1:
+                if node.right:
+                    stack.append([node.right, 0])
+            elif state == 0:
+                if node.left:
+                    stack.append([node.left, 0])
+            subarr[1] += 1
+                    
+        return None
+
+# @lc code=end
+
+    # 官方题解思路：递归（80ms）
+    # https://leetcode-cn.com/problems/lowest-common-ancestor-of-a-binary-tree/solution/er-cha-shu-de-zui-jin-gong-gong-zu-xian-by-leetcod/
+    def lowestCommonAncestor3(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         
         def findAncestor(node):
             if node is None:
@@ -42,8 +78,6 @@ class Solution:
         
         findAncestor(root)
         return self.ans
-
-# @lc code=end
 
     # 递归撒网找pq（148ms）
     # 找到p和q所在的位置，然后倒推计算出最近的公共祖先的位置
@@ -137,5 +171,13 @@ class Solution:
 
         # 如果没有找到公共祖先，返回根节点作为公共祖先
         return root
-        
 
+from lctest import *
+def testCase(treestr, nodestr1, nodestr2):
+    root = stringToTreeNode(treestr)
+    ans = Solution().lowestCommonAncestor(root, TreeNode(int(nodestr1)), TreeNode(int(nodestr2)))
+    return str(ans.val)
+
+if __name__ == "__main__":
+    assert testCase("[3,5,1,6,2,0,8,null,null,7,4]", "5", "1") == "3"
+    assert testCase("[3,5,1,6,2,0,8,null,null,7,4]", "5", "4") == "5"
