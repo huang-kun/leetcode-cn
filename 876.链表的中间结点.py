7f4912880319c61648547c1f6901a60612109c08
huang-kun <jack-huang-developer@foxmail.com>
2020-03-23 09:21:11 +0800

    876.链表的中间结点

---
+++ 876.链表的中间结点.py
@@ -0,0 +1,28 @@
+#
+# @lc app=leetcode.cn id=876 lang=python3
+#
+# [876] 链表的中间结点
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def middleNode(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
+
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        
+        return slow
+        
+# @lc code=end
+
